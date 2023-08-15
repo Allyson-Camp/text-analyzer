709616da8fd20b25af943273551bc20d9faec2e7
Allyson Camp <Allysoncamp92@gmail.com>
2023-08-15 15:55:16 -0700

    correct business logic bug in numberOfOccurancesInText function
    Co-authored-by: Jake Elsberry <jelsberry96@gmail.com>
    Co-authored-by: Onur Kaymak <onurkaymak@outlook.com>
    Co-authored-by: Similoluwa Oyinkoalde <simi.oyinkolade@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,57 @@
 // Business Logic
 function wordCounter(text) {
-  if (text.trim().length === 0) {
-    return 0;
-  }
-  let wordCount = 0;
-  const textArray = text.split(" ");
-  textArray.forEach(function (element) {
-    if (!Number(element)) wordCount++;
-  });
-  return wordCount;
+    if (text.trim().length === 0) {
+        return 0;
+    }
+    let wordCount = 0;
+    const textArray = text.split(" ");
+    textArray.forEach(function (element) {
+        if (!Number(element)) wordCount++;
+    });
+    return wordCount;
 }
 
 function numberOfOccurrencesInText(word, text) {
-  const textArray = text.split(" ");
-  let wordCount = 0;
-  textArray.forEach(function (element) {
-    if (element.toLowerCase().includes(word.toLowerCase())) {
-      wordCount++;
+    if (word.trim().length === 0) {
+        return 0;
     }
-  });
-  return wordCount;
+    const textArray = text.split(" ");
+    let wordCount = 0;
+    textArray.forEach(function (element) {
+        if (element.toLowerCase().includes(word.toLowerCase())) {
+            wordCount++;
+        }
+    });
+    return wordCount;
 }
 
-function swearWords(word, text) {
-  const textArray = text.split(" ");
-  let wordCount = 0;
-  textArray.forEach(function (element) {
-    if (word === "zoinks" || "muppeteer" || "biffaroni" || "loopdaloop") {
-    }
-
-   });
-
-
- 
+function swearWords(text) {
+    const newTextArray = text.split(" ");
+    let wordCount = 0;
+    let variable = text;
+    let badWords = ["zoinks", "muppeteer", "biffaroni", "loopdaloop"];
+    newTextArray.forEach(function (variable) {
+        if (variable.includes(badWords)) {
+            wordCount--;
+        }
+        return newTextArray;
+    });
 }
-   console.log(swearWords("biffaroni","hi! name is 2 the highest my biffaroni muppeteer "));
+
+// UI Logic
+
+function handleFormSubmission(event) {
+    event.preventDefault();
+    const passage = document.getElementById("text-passage").value;
+    const word = document.getElementById("word").value;
+    const wordCount = wordCounter(passage);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    document.getElementById("total-count").innerText = wordCount;
+    document.getElementById("selected-count").innerText = occurrencesOfWord;
+}
+
+window.addEventListener("load", function () {
+    document
+        .querySelector("form#word-counter")
+        .addEventListener("submit", handleFormSubmission);
+});
