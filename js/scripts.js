f6eead49ebb8c3164bd2f7103cbeb783272431c9
Allyson Camp <Allysoncamp92@gmail.com>
2023-08-15 11:29:43 -0700

    add test to look for word regardless of case. working.
    Co-authored-by: Onur Kaymak <onurkaymak@outlook.com>
    Co-authored-by: Jake Elsberry <jelsberry96@gmail.com>
    Co-authored-by: Similoluwa Oyinkoalde <simi.oyinkolade@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -24,5 +24,15 @@ function numberOfOccurrencesInText(word, text) {
 }
 
 
+function numberOfOccurrencesInText(word, text) {
+  const textArray = text.split(" ");
+  let wordCount = 0;
+  textArray.forEach(function(element) {
+    if (word.toLowerCase() === element.toLowerCase()) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
 
  
