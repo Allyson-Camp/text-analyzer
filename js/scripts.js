678fce9e31d9cc63d584a703efb350a6692510aa
Allyson Camp <Allysoncamp92@gmail.com>
2023-08-15 10:41:43 -0700

    add last tdd statement, add function to js. Working.
    Co-authored-by: Jake Elsberry <jelsberry96@gmail.com>
    Co-authored-by: Onur Kaymak <onurkaymak@outlook.com>
    Co-authored-by: Similoluwa Oyinkoalde <simi.oyinkolade@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
 // Business Logic
 function wordCounter(text) {
     if (text.trim().length === 0) {
-      return 0;
+        return 0;
     }
     let wordCount = 0;
     const textArray = text.split(" ");
-    textArray.forEach(function(word) {
-      wordCount++;
+    textArray.forEach(function (word) {
+        if (!Number(element)) wordCount++;
     });
     return wordCount;
-  }
+}
+
+
